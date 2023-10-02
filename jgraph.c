e26a01e63a3269ca76f75c3992f9a2233996d038
Brandon Kammerdiener <kammerdienerb@kammerdienerbm1max.nomads.utk.edu>
2023-10-02 09:40:39 -0400

    highlight request

--- jgraph.c
+++ jgraph.c
@@ -53,6 +53,12 @@ void eline(yed_event *event)  {
 
     yed_syntax_line_event(&syn, event);
 }
+void ehigh(yed_event *event)  {
+    if (event->ft == yed_get_ft("Jgraph")) {
+        yed_syntax_highlight_request_event(&syn, event);
+        event->cancel = 1;
+    }
+}
 
 
 void unload(yed_plugin *self) {
@@ -64,6 +70,7 @@ int yed_plugin_boot(yed_plugin *self) {
     yed_event_handler buffdel;
     yed_event_handler buffmod;
     yed_event_handler line;
+    yed_event_handler high;
 
     char              *edit_kwds[] = {
         "x", "y", "fontsize", "linesep",
@@ -124,6 +131,10 @@ int yed_plugin_boot(yed_plugin *self) {
     line.kind = EVENT_LINE_PRE_DRAW;
     line.fn   = eline;
     yed_plugin_add_event_handler(self, line);
+
+    high.kind = EVENT_HIGHLIGHT_REQUEST;
+    high.fn   = ehigh;
+    yed_plugin_add_event_handler(self, high);
 
 
     SYN();
